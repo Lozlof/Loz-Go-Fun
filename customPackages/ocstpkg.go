cd6ad676a7ee86e1f5c6d6e7ce5603211a4659aa
Justin <lozlofcyber@outlook.com>
2024-10-23 10:59:22 -0400

    Delete customPackages directory

--- customPackages/ocstpkg.go
+++
@@ -1,6 +0,0 @@
-package ocstpkg // Declaring that this file is part of the ocstpkg package.
-
-// Greet is a public function that returns a string.
-func Greet() string{
-	return "Hello from the ocstpkg package! A custom made package!"
-}
