dffd3835181c1117f52349de703ad07745de8032
lozlof <lozlofcyber@gmail.com>
2024-10-24 18:34:00 +0000

    Created executables, put in dir

--- createModule/hello/hello.go
+++ createModule/hello/hello.go
@@ -13,7 +13,7 @@ func main() {
 
 	// Request a greeting message.
 	// Function call.
-	message, err := greetings.Hello("Justin")
+	message, err := greetings.Hello("Stranger")
 	// Calling the Hello function from the greetings package.
 	// ("") (Empty string) Is getting passed to the Hello function.
 	// The Hello function will return its message and error depending on what is passed here.
